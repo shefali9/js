3cd8879d06e5a7e93ac3854f0e47375016198dc9
shefali shrivastava <32157066+shefali9@users.noreply.github.com>
2026-06-02 07:19:39 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,9 +1,9 @@
 let score = "33ea"
-console.log(typeof score)
-console.log(typeof score)
+//console.log(typeof score)
+//console.log(typeof score)
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
 
 
 // "33" =>33
@@ -12,7 +12,7 @@ console.log(valueInNumber)
 
 let isLoggedIn = "33as"
 let booleanLoggedIN = Boolean(isLoggedIn)
-console.log(booleanLoggedIN)
+//console.log(booleanLoggedIN)
 
 
 // 1 => true ; 0 => false
@@ -21,6 +21,41 @@ console.log(booleanLoggedIN)
 
 let someNumber =33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
+//console.log(stringNumber)
+
+
+
+//*********************Operations **************** */
+
+let value =3
+let negValue = -value 
+//console.log(negValue)
+
+
+let str1 = "hi"
+let str2= "shefali"
+let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+//console.log(+true)
+
+//console.log(+"")
+
+let num1, num2 , num3 
+num1 = num2 = num3 = 2+ 2
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study 
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
 
 
