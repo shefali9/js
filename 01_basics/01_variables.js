f71fe0efac6e096c85954f03314bddadf8171a0d
shefali shrivastava <32157066+shefali9@users.noreply.github.com>
2026-06-02 05:39:43 +0000

    variables & datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId =144658
+let accountEmail="shefali@gmail.com"
+var accountPassword ="123456"
+accountCity="gwalior"
+accountEmail ="shef@gmail.com"
+accountPassword = "6779988798"
+accountCity ="Noida"
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity])
