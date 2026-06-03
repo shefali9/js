96f45accfb324ef7a2fcda29c9c9e0841d7dd6bc
shefali shrivastava <32157066+shefali9@users.noreply.github.com>
2026-06-03 11:13:19 +0000

    dateinJS

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,16 @@
+// Dates
+
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(typeof myDate);
+
+
+let myCreatedDate = new Date (2022,3,23,5,3)
+console.log(myCreatedDate.toDateString())
+
+
+
+
