98c763dc063a9568ef5e3866a2a73a1174b2d3cd
shefali shrivastava <32157066+shefali9@users.noreply.github.com>
2026-06-03 08:12:51 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,25 @@
+const name= "shefali"
+const repoCount = 50
+
+//console.log(name + repoCount + "Value");
+
+console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);
+
+const gameName = new String('shefali')
+
+console.log(gameName[0])
+console.log(gameName.__proto__)
+
+console.log(gameName.length);
+
+console.log(gameName.toUpperCase());
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString= gameName.slice(-7,4)
+
+console.log(anotherString);
+
+
+
