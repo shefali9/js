3602edb81e1b0d090bff4ab6deeb8c54e4a0b204
shefali shrivastava <32157066+shefali9@users.noreply.github.com>
2026-06-03 07:28:17 +0000

    datatype summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,49 @@
+// Primitive dataTypes
+
+//7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+const score = 100
+const scoreVlaue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const id= Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+//const bigNumber =3456776787899436786876
+
+
+
+  
+
+
+
+
+
+
+
+
+// Refernce Type (Non primitive)
+
+// Arrays, Objects, Functions
+
+
+
+const heros =["shaktiman","naagraj","doga"]
+let myObj ={
+    name: "shefali",
+    age: "25"
+}
+
+const myFunction = function(){
+    console.log("hello world");
+    
+}
+
+console.log(typeof myFunction)
+
